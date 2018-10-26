f2cc017f27b83ae277a63aeff1cf153c9d120191
Jan Viilma <janviilma@hotmail.com>
2018-10-26 18:48:36 +0300

    add calendar current date

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -4,18 +4,22 @@ import { calendarStyles } from 'app/components/Calendar/styles';
 import '../../../../node_modules/react-calendar/dist/Calendar.less';
 import Calendar from 'react-calendar';
 import Paper from '@material-ui/core/Paper/Paper';
+import UserStore from 'app/stores/UserStore';
+import { inject, observer } from 'mobx-react';
 
 interface DashboardProps extends WithStyles<typeof calendarStyles> {
-
+  userStore?: UserStore;
 }
 
+@inject('userStore')
+@observer
 class CalendarCard extends React.Component<DashboardProps> {
   render() {
-    const { classes } = this.props;
+    const { classes, userStore } = this.props;
     return (
       <>
         <Paper>
-          <Calendar className={classes.root} calendarType="Arabic" />
+          <Calendar className={classes.root} calendarType="Arabic" value={new Date(userStore.currentTime)} />
         </Paper>
       </>
     );
--- src/app/stores/UserStore.ts
+++ src/app/stores/UserStore.ts
@@ -1,20 +1,24 @@
 import { observable } from 'mobx';
 import axios from 'axios';
+import * as moment from 'moment';
 
 class UserStore {
 
-    @observable user: User;
-    @observable personalCode: string;
-    @observable isAuthenticated: boolean;
+  @observable user: User;
+  @observable personalCode: string;
+  @observable isAuthenticated: boolean;
 
-    public doLogIn = async (params: {personalCode: string, password: string}) => {
-        axios.post(`https://jsonplaceholder.typicode.com/users`, { params })
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-            })
-    }
+  public doLogIn = async (params: { personalCode: string, password: string }) => {
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { params })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
 
+  public get currentTime() {
+    return moment().format('LLLL');
+  }
 }
 
 export default UserStore;
