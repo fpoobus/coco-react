c8e9afb608ef4457fe57bcff1a1638577aab92ab
Joonas Lume <joonas.lume@netgroup.ee>
2018-10-26 16:16:34 +0300

    Fix merge errors

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
-import { hot } from 'react-hot-loader';
-import { Route, Router, Switch } from 'react-router';
-import { Root } from 'app/containers/Root';
+import {hot} from 'react-hot-loader';
+import {Route, Router, Switch} from 'react-router';
+import {Root} from 'app/containers/Root';
 import Dashboard from 'app/containers/Dashboard/dashboard';
-import { NewClaimPage } from 'app/pages/NewClaimPage';
+import {NewClaimPage} from 'app/pages/NewClaimPage';
 
 import Login from 'app/containers/Login/Login';
 import CaseForm from "app/pages/CaseForm/CaseForm";
-import {IndexPage} from "app/pages/IndexPage";
 
 // render react DOM
 export const App = hot(module)(({ history }) => (
   <Root>
     <Router history={history}>
       <Switch>
-        <Route exact path="/" component={IndexPage} />
         <Route path="/testing" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route exact path="/" component={Dashboard} />
