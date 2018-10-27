a73bb3ec19fce24fd21e3f92adc2abd0423124dc
martlaus <mart.laus@netgroup.ee>
2018-10-27 12:46:29 +0300

    Add cases list

--- src/app/components/Container/RootContainer.tsx
+++ src/app/components/Container/RootContainer.tsx
@@ -1,47 +1,47 @@
 import * as React from 'react';
-import { Theme, WithStyles } from '@material-ui/core';
+import {Theme, WithStyles} from '@material-ui/core';
 import createStyles from '@material-ui/core/styles/createStyles';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 const styles = (theme: Theme) => createStyles({
-  background: {
-    backgroundImage: `url(${'../../../assets/img/skyline.svg'})`,
-    backgroundPosition: 'center bottom',
-    backgroundRepeat: 'no-repeat',
-    width: '100%',
-    height: '100%',
-    position: 'fixed'
-  },
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    padding: '0px 16px',
-    width: '100%'
-  },
-  container: {
-    maxWidth: 1200,
-    width: '75%'
-  }
+    background: {
+        backgroundImage: `url(${'../../../assets/img/skyline.svg'})`,
+        backgroundPosition: 'center bottom',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        position: 'fixed'
+    },
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '0px 16px',
+        width: '100%'
+    },
+    container: {
+        maxWidth: 1200,
+        width: '75%'
+    }
 });
 
 interface RootContainerProps extends WithStyles<typeof styles> {
-  children: JSX.Element
+    children: JSX.Element
 }
 
 class RootContainer extends React.Component<RootContainerProps> {
 
-  render() {
-    const { classes, children } = this.props;
-    return (
-      <div className={classes.background}>
-        <div className={classes.root}>
-          <div className={classes.container}>
-            {children}
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {classes, children} = this.props;
+        return (
+            <div className={classes.background}>
+                <div className={classes.root}>
+                    <div className={classes.container}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(RootContainer);
