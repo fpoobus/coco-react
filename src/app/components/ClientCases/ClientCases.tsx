5aabcea260da7227bbf7b4ef21491ff87bac1e46
Jan Viilma <janviilma@hotmail.com>
2018-10-27 15:27:18 +0300

    complete judgment form

--- src/app/components/ClientCases/ClientCases.tsx
+++ src/app/components/ClientCases/ClientCases.tsx
@@ -1,85 +1,85 @@
 import * as React from 'react';
-import withStyles, {WithStyles} from '@material-ui/core/styles/withStyles';
-import {clientCasesStyles} from 'app/components/ClientCases/styles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import { clientCasesStyles } from 'app/components/ClientCases/styles';
 import Paper from '@material-ui/core/Paper/Paper';
 import Table from '@material-ui/core/Table/Table';
 import TableHead from '@material-ui/core/TableHead/TableHead';
 import TableCell from '@material-ui/core/TableCell/TableCell';
 import TableBody from '@material-ui/core/TableBody/TableBody';
 import TableRow from '@material-ui/core/TableRow/TableRow';
-import CaseStore from "app/stores/CaseStore";
-import {inject, observer} from "mobx-react";
-import Button from "@material-ui/core/Button/Button";
-import Typography from "@material-ui/core/es/Typography/Typography";
-import Grid from "@material-ui/core/Grid/Grid";
-import {Link} from 'react-router-dom';
+import CaseStore from 'app/stores/CaseStore';
+import { inject, observer } from 'mobx-react';
+import Button from '@material-ui/core/Button/Button';
+import Typography from '@material-ui/core/es/Typography/Typography';
+import Grid from '@material-ui/core/Grid/Grid';
+import { Link } from 'react-router-dom';
 
 
 interface ClientCasesProps extends WithStyles<typeof clientCasesStyles> {
-    caseStore?: CaseStore;
+  caseStore?: CaseStore;
 }
 
 @inject('caseStore')
 @observer
 class ClientCases extends React.Component<ClientCasesProps> {
-    newClaimLink = (props, id) => <Link to={"/case?id=" + id} {...props} />;
+  newClaimLink = (props, id) => <Link to={'/case?id=' + id} {...props} />;
 
-    renderTableBody = () => {
-        const {caseStore, classes} = this.props;
+  renderTableBody = () => {
+    const { caseStore } = this.props;
 
-        const caseData = caseStore.casesData;
-        return (
-            caseData.map((courtCase, idx) => {
-                    return <TableRow key={idx}>
-                        <TableCell>{courtCase.status}</TableCell>
-                        <TableCell>{courtCase.type}</TableCell>
-                        <TableCell>{courtCase.claimantId}</TableCell>
-                        <TableCell>{courtCase.defendantId}</TableCell>
-                        <TableCell>{courtCase.description}</TableCell>
-                        <TableCell>
-                            <Button variant="contained" className={classes.btn} component={props => this.newClaimLink(props, courtCase.id)} color="primary">
-                                Go to case
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                }
-            )
-        );
-    };
+    const caseData = caseStore.casesData;
+    return (
+      caseData.map((courtCase, idx) => {
+          return <TableRow key={idx}>
+            <TableCell>{courtCase.status}</TableCell>
+            <TableCell>{courtCase.type}</TableCell>
+            <TableCell>{courtCase.claimantId}</TableCell>
+            <TableCell>{courtCase.defendantId}</TableCell>
+            <TableCell>{courtCase.description}</TableCell>
+            <TableCell>
+              <Button variant="contained" component={props => this.newClaimLink(props, courtCase.id)} color="primary">
+                Go to case
+              </Button>
+            </TableCell>
+          </TableRow>;
+        }
+      )
+    );
+  };
 
-    constructor(props: ClientCasesProps) {
-        super(props);
-        this.props.caseStore.loadCases();
-    }
+  constructor(props: ClientCasesProps) {
+    super(props);
+    this.props.caseStore.loadCases();
+  }
 
-    render() {
-        return (
-            <>
-                <Paper>
-                    <Grid container spacing={24}>
-                        <Grid item xs={12}>
-                            <Typography variant="h5" gutterBottom style={{marginLeft: 20}}>
-                                New cases
-                            </Typography>
+  render() {
+    return (
+      <>
+        <Paper>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <Typography variant="h5" gutterBottom style={{ marginLeft: 20 }}>
+                New cases
+              </Typography>
 
-                        </Grid>
+            </Grid>
 
-                    </Grid>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                {['Status', 'Type', 'Calimant', 'Defendant', 'Description', '']
-                                    .map((title, idx) => <TableCell key={title + idx.toString()}>{title}</TableCell>)}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {this.renderTableBody()}
-                        </TableBody>
-                    </Table>
-                </Paper>
-            </>
-        );
-    }
+          </Grid>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {['Status', 'Type', 'Calimant', 'Defendant', 'Description', '']
+                  .map((title, idx) => <TableCell key={title + idx.toString()}>{title}</TableCell>)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.renderTableBody()}
+            </TableBody>
+          </Table>
+        </Paper>
+      </>
+    );
+  }
 }
 
 export default withStyles(clientCasesStyles)(ClientCases);
