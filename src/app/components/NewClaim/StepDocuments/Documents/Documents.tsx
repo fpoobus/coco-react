bfe3d97bd4effe737f05b6ab28267743f0701ddf
Fredi Poobus <fredi.poobus@netgroup.ee>
2018-10-27 16:17:58 +0300

    Ui impr

--- src/app/components/NewClaim/StepDocuments/Documents/Documents.tsx
+++ src/app/components/NewClaim/StepDocuments/Documents/Documents.tsx
@@ -3,6 +3,11 @@ import {inject, observer} from "mobx-react";
 import NewClaimStore from "app/stores/NewClaimStore";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
 
 export interface DocumentsProps {
     newClaimStore: NewClaimStore
@@ -26,8 +31,21 @@ export class Documents extends React.Component<DocumentsProps, DocumentsState> {
             <Typography component="h1" variant="h5">
                 Please attach the documents relevant to the claim being submitted.
             </Typography>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>File Name</TableCell>
+                        <TableCell numeric>Size</TableCell>
+                        <TableCell numeric>Modified</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                </TableBody>
+            </Table>
 
             <input id="contained-button-file" style={input} multiple type="file"/>
+            <br/>
+            <br/>
             <label htmlFor="contained-button-file">
                 <Button variant="contained" component="span">
                     Upload
