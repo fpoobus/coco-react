4162d9aa1d8e851947a2dbf74b4ec5fcb30be52a
Heleri Mändla <heleri.mandla@netgroup.ee>
2018-10-27 11:07:53 +0300

    minor edits

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -9,46 +9,44 @@ import FaceIcon from '@material-ui/icons/Face';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+export const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      marginBottom: '64px'
+    },
+    bar: {
+      backgroundColor: theme.palette.primary.dark
+    },
+    typo: {
+      color: theme.palette.primary.light,
+      margin: '0 4px'
+    },
+    justifyCenter: {
+      display: 'flex',
+      justifyContent: 'space-between'
+    },
+    typoContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      width: '6%'
+    },
+    indicator: {
+      backgroundColor: '#e8d598'
+    },
+    colorWhite: {
+      color: '#fff'
+    }
+  });
 
-
-export const styles = (theme: Theme) => createStyles({
-  root: {
-    marginBottom: '64px'
-  },
-  bar: {
-    backgroundColor: theme.palette.primary.dark
-  },
-  typo: {
-    color:theme.palette.primary.light,
-    margin:'0 4px'
-  },
-  justifyCenter: {
-    display:'flex',
-    justifyContent: 'space-between'
-  },
-  typoContainer: {
-    display:'flex',
-    justifyContent: 'space-between',
-    width:'6%',
-  },
-  indicator:{
-    backgroundColor:'#e8d598'
-  },
-  colorWhite:{
-    color: '#fff'
-  }
-});
-
-interface HeaderProps extends WithStyles<typeof styles> {
-}
+interface HeaderProps extends WithStyles<typeof styles> {}
 
 class Header extends React.Component<HeaderProps> {
   state = {
     anchorEl: null,
-    value:0
+    value: 0
   };
 
-  handleClick = event => {
+  handleClick = (event) => {
     this.setState({ anchorEl: event.currentTarget });
   };
 
@@ -60,33 +58,36 @@ class Header extends React.Component<HeaderProps> {
     this.setState({ value });
   };
 
-
-
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
         <AppBar className={classes.bar} position="static">
-        <Toolbar className={classes.justifyCenter}>
-        <MenuItem className={classes.typoContainer}>
-           <AccountBalance className={classes.typo}
-             aria-owns={this.state.anchorEl ? 'simple-menu' : null}
-          aria-haspopup="true"
-          onClick={this.handleClick}/>
-        </MenuItem>
-        <Tabs value={this.state.value} onChange={this.handleChange}  classes={{
-    indicator: classes.indicator
-  }}>>
-            <Tab label="Dashboard" className={classes.colorWhite} />
-            <Tab label="Claims" className={classes.colorWhite}/>
-
-          </Tabs>
-          <MenuItem className={classes.typoContainer}>
-          <Typography className={classes.typo}>LOG OUT</Typography>
-          <FaceIcon className={classes.typo}/>
-          </MenuItem>
-        
-        </Toolbar>
+          <Toolbar className={classes.justifyCenter}>
+            <MenuItem className={classes.typoContainer}>
+              <AccountBalance
+                className={classes.typo}
+                aria-owns={this.state.anchorEl ? 'simple-menu' : null}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+              />
+            </MenuItem>
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              classes={{
+                indicator: classes.indicator
+              }}
+            >
+              
+              <Tab label="Dashboard" className={classes.colorWhite} />
+              <Tab label="Claims" className={classes.colorWhite} />
+            </Tabs>
+            <MenuItem className={classes.typoContainer}>
+              <Typography className={classes.typo}>LOG OUT</Typography>
+              <FaceIcon className={classes.typo} />
+            </MenuItem>
+          </Toolbar>
         </AppBar>
         {/* Will be used for mobile view later */}
         {/* <Menu
