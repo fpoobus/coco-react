48f87351f7e2a3fbda78b8b5a143397eef39f70b
Fredi Poobus <fredi.poobus@netgroup.ee>
2018-11-06 23:19:08 +0200

    Fix case type layout

--- src/app/components/NewClaim/StepClaimInfo/ClaimInformation/ClaimInformation.tsx
+++ src/app/components/NewClaim/StepClaimInfo/ClaimInformation/ClaimInformation.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { inject, observer } from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import NewClaimStore from 'app/stores/NewClaimStore';
 import TextField from '@material-ui/core/TextField';
-import { runInAction } from 'mobx';
+import {runInAction} from 'mobx';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
@@ -12,166 +12,171 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 
 export interface ClaimInformationProps {
-  newClaimStore: NewClaimStore
+    newClaimStore: NewClaimStore
 }
 
 export interface ClaimInformationState {
-  /* empty */
+    /* empty */
 }
 
 
 let gridItem = {
-  margin: '10px'
+    margin: '10px'
 };
 
+let textCenter = {
+    textAlign: 'center' as 'center'
+};
 
 @inject('routerStore', 'newClaimStore')
 @observer
 export class ClaimInformation extends React.Component<ClaimInformationProps, ClaimInformationState> {
 
-  state = {
-    value: ''
-  };
+    state = {
+        value: ''
+    };
 
-  renderClaimInfo() {
-    let claim = this.props.newClaimStore.newClaim.claim;
-    this.state.value = claim.case_type;
+    renderClaimInfo() {
+        let claim = this.props.newClaimStore.newClaim.claim;
+        this.state.value = claim.case_type;
 
-    return <>
-      <Grid container justify="center">
-        <Grid item>
-          <Typography component="h2" variant="h4" gutterBottom>
-            Select Case Type
-          </Typography>
-          <Typography component="subtitle1" gutterBottom>
-            Depending on the case type the State Fee will be different.
-          </Typography>
-          <Divider light />
-          <br /><br />
-        </Grid>
-      </Grid>
-      <Grid container justify="center">
-        <Grid item xs={3} sm={3}>
-
-          <Paper style={gridItem} elevation={1}>
-
-
-            <Grid justify="center">
-              <Grid item>
-
-                <RadioGroup
-                  aria-label="Case Type"
-                  name="claim_type"
-                  value={this.state.value}
-                  onChange={this.setCaseType}
-                >
-                  <FormControlLabel value="bankruptcy" control={<Radio />} label="Bankruptcy" />
-                </RadioGroup>
-
-                <Typography variant="subtitle1" gutterBottom>
-                  State Fee: 30 USD
-                </Typography>
-              </Grid>
-            </Grid>
-          </Paper>
-
-        </Grid>
-
-        <Grid xs={3} sm={3}>
-
-          <Paper style={gridItem} elevation={1}>
-
+        return <>
             <Grid container justify="center">
-              <Grid item>
-
-                <RadioGroup
-                  aria-label="Case Type"
-                  name="claim_type"
-                  value={this.state.value}
-                  onChange={this.setCaseType}
-                >
-                  <FormControlLabel value="contract-violation" control={<Radio />}
-                                    label="Contract Violation" />
-                </RadioGroup>
-
-                <Typography variant="subtitle1" gutterBottom>
-                  State Fee: 80 USD
-                </Typography>
-              </Grid>
+                <Grid item>
+                    <Typography style={textCenter} component="h2" variant="h4" gutterBottom>
+                        Select Case Type
+                    </Typography>
+                    <Typography component="subtitle1" gutterBottom>
+                        Depending on the case type the State Fee will be different.
+                    </Typography>
+                    <Divider light/>
+                    <br/><br/>
+                </Grid>
             </Grid>
-          </Paper>
-        </Grid>
+            <Grid container justify="center">
 
 
-          <Grid xs={3} sm={3}>
+                <Grid xs={3} sm={3}>
 
-              <Paper style={gridItem} elevation={1}>
+                    <Paper style={gridItem} elevation={1}>
 
-                  <Grid container justify="center">
-                      <Grid item>
+                        <Grid container justify="center">
+                            <Grid item>
 
-                          <RadioGroup
-                              aria-label="Case Type"
-                              name="claim_type"
-                              value={this.state.value}
-                              onChange={this.setCaseType}
-                          >
-                              <FormControlLabel value="equitable-claim" control={<Radio />}
-                                                label="Equitable Claim" />
-                          </RadioGroup>
+                                <RadioGroup
+                                    aria-label="Case Type"
+                                    name="claim_type"
+                                    value={this.state.value}
+                                    onChange={this.setCaseType}
+                                >
+                                    <FormControlLabel value="bankruptcy" control={<Radio/>}
+                                                      label="Bankruptcy"/>
+                                </RadioGroup>
 
-                          <Typography variant="subtitle1" gutterBottom>
-                              State Fee: 80 USD
-                          </Typography>
-                      </Grid>
-                  </Grid>
-              </Paper>
-          </Grid>
-
-      </Grid>
+                                <Typography variant="subtitle1" gutterBottom>
+                                    State Fee: 30 USD
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
 
 
-      <TextField
-        id="outlined-multiline-flexible"
-        label="Claim Summary"
-        multiline
-        fullWidth
-        rows="5"
-        value={claim.description}
-        onChange={this.handleChange('description')}
-        margin="normal"
-        variant="outlined"
-      />
+                <Grid xs={3} sm={3}>
 
-    </>;
-  }
+                    <Paper style={gridItem} elevation={1}>
 
-  handleChange = name => event => {
-    runInAction(() => {
-      this.props.newClaimStore.newClaim.claim[name] = event.target.value;
+                        <Grid container justify="center">
+                            <Grid item>
 
-    });
-  };
+                                <RadioGroup
+                                    aria-label="Case Type"
+                                    name="claim_type"
+                                    value={this.state.value}
+                                    onChange={this.setCaseType}
+                                >
+                                    <FormControlLabel value="contract-violation" control={<Radio/>}
+                                                      label="Contract Violation"/>
+                                </RadioGroup>
 
-  setCaseType = event => {
-    console.log('asd');
+                                <Typography variant="subtitle1" gutterBottom>
+                                    State Fee: 80 USD
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
 
-    if (event.target.value === 'contract-violation') {
-      this.props.newClaimStore.newClaim.fee.fee = '80';
-    } else {
-      this.props.newClaimStore.newClaim.fee.fee = '30';
+
+                <Grid xs={3} sm={3}>
+
+                    <Paper style={gridItem} elevation={1}>
+
+                        <Grid container justify="center">
+                            <Grid item>
+
+                                <RadioGroup
+                                    aria-label="Case Type"
+                                    name="claim_type"
+                                    value={this.state.value}
+                                    onChange={this.setCaseType}
+                                >
+                                    <FormControlLabel value="equitable-claim" control={<Radio/>}
+                                                      label="Equitable Claim"/>
+                                </RadioGroup>
+
+                                <Typography variant="subtitle1" gutterBottom>
+                                    State Fee: 80 USD
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+
+            </Grid>
+
+
+            <TextField
+                id="outlined-multiline-flexible"
+                label="Claim Summary"
+                multiline
+                fullWidth
+                rows="5"
+                value={claim.description}
+                onChange={this.handleChange('description')}
+                margin="normal"
+                variant="outlined"
+            />
+
+        </>;
     }
 
-    this.setState({ value: event.target.value });
-    runInAction(() => {
-      this.props.newClaimStore.newClaim.claim.case_type = event.target.value;
-      console.log(this.props.newClaimStore.newClaim.claim.case_type);
-    });
-  };
+    handleChange = name => event => {
+        runInAction(() => {
+            this.props.newClaimStore.newClaim.claim[name] = event.target.value;
 
-  render() {
-    return this.renderClaimInfo();
-  }
+        });
+    };
+
+    setCaseType = event => {
+        console.log('asd');
+
+        if (event.target.value === 'contract-violation') {
+            this.props.newClaimStore.newClaim.fee.fee = '80';
+        } else {
+            this.props.newClaimStore.newClaim.fee.fee = '30';
+        }
+
+        this.setState({value: event.target.value});
+        runInAction(() => {
+            this.props.newClaimStore.newClaim.claim.case_type = event.target.value;
+            console.log(this.props.newClaimStore.newClaim.claim.case_type);
+        });
+    };
+
+    render() {
+        return this.renderClaimInfo();
+    }
 }
 
 export default ClaimInformation;
